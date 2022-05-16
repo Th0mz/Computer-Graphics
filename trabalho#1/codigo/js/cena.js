4df6805f92c397231480f5310746b2d881fc1715
Tomás Tavares <57302012+Th0mz@users.noreply.github.com>
2022-05-16 13:53:17 +0100

    basic scene components added

--- trabalho#1/codigo/js/cena.js
+++ trabalho#1/codigo/js/cena.js
@@ -6,16 +6,24 @@
 *
 */
 
-function onResize() {
-    'use strict';
+// TODO : dont use global variables
+var camera, scene, renderer;
 
-    /*renderer.setSize(window.innerWidth, window.innerHeight);
+var geometry, material, mesh;
+
+var ball;
+
+
+function onResize() {
+    'use strict'
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
     if (window.innerHeight > 0 && window.innerWidth > 0) {
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
     }
-    */
+
 }
 
 function onKeyDown(e) {
@@ -24,12 +32,49 @@ function onKeyDown(e) {
 
 function render() {
     'use strict';
+    ball.position.x += 0.1;
+    renderer.render(scene, camera);
 }
 
 function init() {
     'use strict';
+
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    scene = new THREE.Scene();
+    scene.add(new THREE.AxisHelper(10));
+
+
+    camera = new THREE.PerspectiveCamera(70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000);
+
+    camera.position.x = 50;
+    camera.position.y = 50;
+    camera.position.z = 50;
+    camera.lookAt(scene.position);
+    
+    ball = new THREE.Object3D();
+
+    material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+    geometry = new THREE.SphereGeometry(4, 10, 10);
+    mesh = new THREE.Mesh(geometry, material);
+
+    ball.add(mesh);
+    ball.position.set(0, 0, 0);
+
+    scene.add(ball);
+
+    window.addEventListener("resize", onResize);
 }
 
 function animate() {
     'use strict';
+    render();
+    requestAnimationFrame(animate);
 }
