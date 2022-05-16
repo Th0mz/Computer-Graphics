0ce33b20a539227604ff66f7779a304111355871
Tomás Tavares <57302012+Th0mz@users.noreply.github.com>
2022-05-16 10:33:16 +0100

    setup do código

---
+++ trabalho#1/codigo/js/cena.js
@@ -0,0 +1,35 @@
+/*
+* Trabalho # 1
+*   95535 - António Coelho
+*   95549 - Cristi Savin 
+*   95680 - Tomás Tavares 
+*
+*/
+
+function onResize() {
+    'use strict';
+
+    /*renderer.setSize(window.innerWidth, window.innerHeight);
+
+    if (window.innerHeight > 0 && window.innerWidth > 0) {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    }
+    */
+}
+
+function onKeyDown(e) {
+    'use strict';
+}
+
+function render() {
+    'use strict';
+}
+
+function init() {
+    'use strict';
+}
+
+function animate() {
+    'use strict';
+}
