51fe9273d760c790ebbd3677831e1ce2d2fb7c31
antonioncoelho <coelho_amsnc@hotmail.com>
2022-05-17 19:03:17 +0100

    added new shapes(1 cube, 1 cylinder, 2 cones)

--- trabalho#1/codigo/js/cena.js
+++ trabalho#1/codigo/js/cena.js
@@ -80,6 +80,19 @@ class Cylinder {
 
 }
 
+class Cone {
+    constructor (radius, height, radialSegments, _color=0xADD8E6, _wireframe=true) {
+        var cone = new THREE.Object3D();
+        var material = new THREE.MeshBasicMaterial({color: _color, wireframe: _wireframe});
+        const geometry = new THREE.ConeGeometry(radius, height, radialSegments);
+        var mesh = new THREE.Mesh(geometry, material);
+
+        return cone.add(mesh);
+    }
+}
+
+
+
 
 function onResize() {
     'use strict'
@@ -135,7 +148,7 @@ function render() {
 
 function init() {
     'use strict';
-    var ball_1, ball_2, ball_3, torus, cube, pyramid_1, pyramid_2, cylinder;
+    var ball_1, ball_2, ball_3, torus, cube, cube2, pyramid_1, pyramid_2, cylinder, cylinder2, cone1, cone2;
 
     renderer = new THREE.WebGLRenderer({
         antialias: true
@@ -190,14 +203,35 @@ function init() {
     cylinder.rotateX(Math.PI * 0.5);
     cylinder.position.set(2.5 * side_size, -0.5 * side_size, 0);
 
+    cone1 = new Cone(1 * side_size, 4 * side_size, 16);
+    cone1.rotateX(Math.PI/2);
+    cone1.position.set(-2 * side_size, -3 * side_size, -1 * side_size);
+
+    cone2 = new Cone(1 * side_size, 1 * side_size, 16);
+    cone2.rotateX(Math.PI/2);
+    cone2.rotateZ(3 * (Math.PI/2));
+    cone2.position.set(-0.5 * side_size, 0, 0);
+
+    cube2 = new Cube(2 * side_size);
+    cube2.position.set(6 * side_size, 0, 0);
+
+    cylinder2 = new Cylinder(1 * side_size, 1 * side_size, 16);
+    cylinder2.rotateX(Math.PI/2);
+    cylinder2.position.set(6 * side_size, 0, 1.5 * side_size);
+
+
     scene.add(ball_1);
     scene.add(ball_2);
     scene.add(ball_3);
     scene.add(torus);
     scene.add(cube);
+    scene.add(cube2);
     scene.add(pyramid_1);
     scene.add(pyramid_2);
     scene.add(cylinder);
+    scene.add(cylinder2);
+    scene.add(cone1);
+    scene.add(cone2);
 
     console.log(scene.position);
 
