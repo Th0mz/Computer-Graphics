8c602712f30e85e333d464053c4246d9e3348a39
Tomás Tavares <57302012+Th0mz@users.noreply.github.com>
2022-06-08 11:25:15 +0100

    base code for trabalho 3

---
+++ trabalho3/code/js/primitives.js
@@ -0,0 +1,74 @@
+function createObject (x, y, z, geometry, _color, rotX=0, rotY=0, rotZ=0, _wireframe=true) {
+    var object = new THREE.Object3D();
+    var material = new THREE.MeshBasicMaterial({ color: _color, wireframe: _wireframe });
+    var mesh = new THREE.Mesh(geometry, material);
+
+    object.add(mesh);
+    object.rotateX(rotX);
+    object.rotateY(rotY);
+    object.rotateZ(rotZ);
+    object.position.set(x , y , z );
+    
+    scene.add(object);
+    return object;
+}
+
+function createGlobe (x, y, z, diameter, _color, _wireframe=true) {
+    var geometry = new THREE.SphereGeometry((diameter / 2) , 32, 16);
+
+    var texture = new THREE.TextureLoader().load('assets/2k_mars.jpg');
+
+    var object = new THREE.Object3D();
+    var material = new THREE.MeshBasicMaterial({ map: texture, wireframe: _wireframe });
+    var mesh = new THREE.Mesh(geometry, material);
+
+    object.add(mesh);
+    object.rotateX(0);
+    object.rotateY(0);
+    object.rotateZ(0);
+    object.position.set(x , y , z );
+    
+    scene.add(object);
+
+    return object;
+}
+
+function createSphere (x, y, z, diameter, _color, _wireframe=true) {
+    
+    var geometry = new THREE.SphereGeometry((diameter / 2) , 32, 16);
+    var sphere = createObject(x, y, z, geometry, _color, 0, 0, 0, _wireframe);
+
+    return sphere;
+}
+
+function createCube(x, y, z, size, _color, _wireframe=true) {
+
+    var geometry = new THREE.BoxGeometry(size , size , size );
+    var cube = createObject(x, y, z, geometry, _color, 0, 0, 0, _wireframe);
+
+    return cube;
+}
+
+function createPyramid(x, y, z, diameter, height, base_polygon,  _color, _wireframe=true, rotX = 0, rotY = 0, rotZ = 0) {
+    
+    var geometry = new THREE.ConeGeometry((diameter / 2) , height , base_polygon);
+    var pyramid = createObject(x, y, z, geometry, _color, rotX, rotY, rotZ, _wireframe);
+    
+    return pyramid;
+}
+
+function createCapsule (x, y, z, diameter, height, _color, _wireframe, rotX = 0, rotY = 0, rotZ = 0) {
+    
+    var geometry = new THREE.CapsuleGeometry( (diameter / 2) , height , 10, 10 ); 
+    var capsule = createObject(x, y, z, geometry, _color, rotX, rotY, rotZ, _wireframe);
+    
+    return capsule;
+}
+
+function createCylinder (x, y, z, diameter, height, base_polygon, _color, _wireframe=true, rotX = 0, rotY = 0, rotZ = 0) {
+    
+    var geometry = new THREE.CylinderGeometry( (diameter / 2) , (diameter / 2) , height , base_polygon);
+    var cylinder = createObject(x, y, z, geometry, _color, rotX, rotY, rotZ, _wireframe);
+    
+    return cylinder;
+}
---
+++ trabalho3/code/js/scene.js
@@ -0,0 +1,118 @@
+/*
+* Trabalho # 2
+*   95535 - António Coelho
+*   95549 - Cristi Savin 
+*   95680 - Tomás Tavares 
+*
+*/
+
+// Cameras
+var mainCamera;
+var frontalCamera;
+
+var scene, renderer;
+
+// Camera resize variables
+var originalAspect;
+var viewSize;
+
+// Scene objects properties
+var side_size = 10;
+const R = 30;
+
+// Global clock
+var clock = new THREE.Clock();
+
+function createCameras () {
+
+    viewSize = 800;
+    var aspectRatio = window.innerWidth / window.innerHeight;
+    originalAspect = window.innerWidth / window.innerHeight;
+
+    // Frontal Camera
+    frontalCamera = new THREE.OrthographicCamera(-aspectRatio * viewSize / 2, 
+                                          aspectRatio * viewSize / 2,
+                                          viewSize / 2, 
+                                          -viewSize / 2,
+                                          0.1,
+                                          5000 );
+
+    frontalCamera.position.set(0, 0, 1000);
+    frontalCamera.lookAt(scene.position);
+
+    // Set main camera
+    mainCamera = frontalCamera;
+}
+
+function createScene () {
+    
+    scene = new THREE.Scene();
+    var sphere = createSphere(0, 0, 0, 100, 0x00ff00);
+    
+    scene.add(sphere);
+}
+
+function onResize() {
+    'use strict'
+    var aspect = window.innerWidth / window.innerHeight;
+    var change = originalAspect / aspect;
+    var newSize = viewSize * change;
+    
+    // Frontal Camera Resize
+    frontalCamera.left = -aspect * newSize / 2;
+    frontalCamera.right = aspect * newSize  / 2;
+    frontalCamera.top = newSize / 2;
+    frontalCamera.bottom = -newSize / 2;
+    frontalCamera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+function onKeyDown(e) {
+    'use strict';
+    switch(e.keyCode) {
+    }
+}
+
+function onKeyUp(e){
+    'use strict';
+    switch(e.keyCode) {
+    }
+
+}
+
+function render() {
+    'use strict';
+    renderer.render(scene, mainCamera);
+}
+
+function init() {
+    'use strict';
+    
+    renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha : true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    
+    createScene();
+    createCameras();
+
+    
+    window.addEventListener("resize", onResize);
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("keyup", onKeyUp);
+}
+
+function animate() {
+    'use strict';
+
+    var delta_time = clock.getDelta();
+    render();
+
+    setTimeout( function() {
+        requestAnimationFrame( animate );
+    }, 1000 / 60 );
+}
